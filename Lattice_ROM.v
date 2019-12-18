d9a838ef3498436b7a87db330a2fc69e3a19f1e5
平凡的纸袋鼠 <1554287628@qq.com>
2019-12-18 23:36:53 +0800

    do nething, go to sleep

---
+++ Lattice_ROM.v
@@ -0,0 +1,18 @@
+module Lattice_ROM(clk, outaddr, dout);  //用寄存器实现的点阵ROM
+	input clk; 
+	input [11:0] outaddr;
+	output reg [11:0] dout;
+	
+	reg [11:0] rom [4095:0]; 
+	
+	initial 
+	begin
+		$readmemh("E:/Final-DC-exp/mem.txt", rom); 
+	end
+	
+	always @(posedge clk) 
+	begin 
+		dout<=rom[outaddr];
+	end 
+
+endmodule 
