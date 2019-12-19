26e6ec7494ab66dfbb7866cc8afc0886e2d95441
youngstudent2 <644699280@qq.com>
2019-12-19 16:53:51 +0800

    new char structure

--- Generator.v
+++ Generator.v
@@ -1,12 +1,14 @@
-module Generator(clk,newCh,speed,y);
+module Generator(clk,ch,speed,x,y);
     input clk;
-    output [22:0] newCh;
+	 
+	 output [7:0] ch;
+    output [3:0] speed;
+	 output [8:0] x;
+    output [9:0] y;
 
-    output wire [11:0] speed;
 	 wire [11:0] ran_y;
 	 wire [11:0] div_y;
-    output [11:0] y;
-
+	 
     random random_speed(
         .clk(clk),
         .rst(1'b0),
@@ -20,13 +22,20 @@ module Generator(clk,newCh,speed,y);
         .seed(12'd320),
         .randomNum(ran_y)
     );
+	 
+	 random random_ch(
+			.clk(clk).
+			.rst(1'b0),
+			.seed(12'd0),
+			.randomNum({4'b0000,ch})
+	 );
 	
-	
+	 
 	 assign div_y = {6'b000000,{2'b00,ran_y[11:9]}+ran_y[11:7]};
     assign y = ran_y - div_y<<7 - div_y<<9;  //y % 640
    
+	 assign x = 9'd0;
 
-    assign newCh = {speed[3:0],9'd0,y[9:0]};
 
 
 endmodule
