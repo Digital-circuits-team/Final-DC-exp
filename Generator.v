220dc1b53862cc3891e265b02d1e870be79f6231
youngstudent2 <42881835+youngstudent2@users.noreply.github.com>
2019-12-19 15:54:59 +0800

    Update Generator.v

--- Generator.v
+++ Generator.v
@@ -1,8 +1,19 @@
 module(clk,newCh);
     input clk;
-    output [23:0] randomNum;
+    output [23:0] newCh;
 
-    
+    reg [11:0] speed,y;
+
+    random random_speed(
+        .clk(clk),
+        .rst(1'b0),
+        .seed(12'd315),
+        .randomNum(speed)
+    );
+
+    always @
+
+    assign newCh = {1'b1,speed,9'd0,y};
 
 
 endmodule
