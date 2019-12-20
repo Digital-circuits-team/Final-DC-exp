3a04b8845dab96b2ca6fe227d641c1bf8fae0a80
youngstudent2 <644699280@qq.com>
2019-12-20 19:59:45 +0800

    Update Game.v

--- Game.v
+++ Game.v
@@ -47,12 +47,17 @@ module Game(
 
 );
 
+	parameter lower_bound = 10'd480;
+	parameter white = 24'hFFFFFF;
+	parameter black = 24'h000000;
+	
+	
 	//out_clk
 	wire generator_clk;
 	
 	//Generator
 	wire [7:0]char;
-	wire [3:0]tmp_speed;
+	wire [2:0]tmp_speed;
 	wire [8:0]tmp_x;
 	wire [9:0]tmp_y;
 	
@@ -77,7 +82,7 @@ module Game(
 	reg flag;
 	//other
 	reg [9:0] offset[639:0]; //行偏移量
-	reg [3:0] speed[639:0];  //速度
+	reg [2:0] speed[639:0];  //速度
 	reg [9:0] h_offset;  //字符内列信息，防止溢出故设为10位
 	reg [3:0] v_offset;  //字符内行信息
 	reg [639:0] columnTable;  //判断某一列是否有字符
@@ -86,8 +91,10 @@ module Game(
 	reg [18:0] countclk;
 	reg [5:0] count;
 	reg moveable;  //每隔一定周期让字符下滑
+	reg gameover;  //游戏结束标志
 	
 	initial begin
+		gameover = 1'b0;
 		count=6'd0;
 		countclk=19'd0;
 		moveable=1'b0;
@@ -96,7 +103,7 @@ module Game(
 		columnTable=640'b0;
 		flag=1'b0;
 	end
-
+	assign LEDR[4] = gameover;
 	//生成vga_clk
 	clkgen #(25000000) my_vgaclk(
 		.clkin(CLOCK_50), 
@@ -151,7 +158,7 @@ module Game(
 	//生成moveable
 	always @ (posedge VGA_CLK) begin 
 		countclk=countclk+1'b1;
-		if(countclk==19'd420000)begin
+		if(countclk==19'd420000)begin//
 			count=count+6'd1;
 			countclk=19'd0;
 		end
@@ -189,28 +196,33 @@ module Game(
 	
 	always @ (posedge VGA_CLK) begin  //字符产生及下滑
 		if(generator_clk)begin  //生成字符，设置offset和speed；TODO:可能会覆盖，待修改
-			offset[tmp_y][8:0]<=tmp_x;
-			speed[tmp_y][3:0]<=tmp_speed;
-			columnTable[tmp_y]<=1'b1;
-			if(moveable==1'b1&&h_offset==10'd0&&v_addr==10'd0)begin
-				offset[charIndex]<=offset[charIndex]+speed[charIndex];
-			end
-			else begin offset[charIndex]<=offset[charIndex]; end
+			offset[tmp_y][8:0]=tmp_x;
+			speed[tmp_y][2:0]=tmp_speed;
+			columnTable[tmp_y]=1'b1;
 		end
-		else begin
+		else begin//字符移动
 			if(moveable==1'b1&&h_offset==10'd0&&v_addr==10'd0)begin
-				offset[charIndex]<=offset[charIndex]+speed[charIndex];
+				offset[charIndex]=offset[charIndex]+speed[charIndex];
+								/*
+				if(offset[charIndex]>=lower_bound) begin //字符到达屏幕底部，游戏结束
+					gameover = 1'b1;
+					//临时处理字符到达屏幕底部情况
+					offset[charIndex] = 3'd0;
+					speed[charIndex] = 10'd0;				
+				end*/
 			end
-			else begin offset[charIndex]<=offset[charIndex]; end
+			else begin 
+				offset[charIndex]=offset[charIndex];
+			end
 		end
 	end
 	
 	always @ (posedge VGA_CLK) begin   //设置vga_data，显示
 			if(flag==1'b1&&(color_bit>>h_offset)&12'h001 == 1'b1)begin  //取出的一位bit信息为1 
-				vga_data = 24'hffffff;  //black
+				vga_data <= black;  //black
 			end
 			else begin
-				vga_data = 24'h000000;
+				vga_data <= white;
 			end
 	end
 
