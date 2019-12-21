ca09270300d50d3d7e076aabae36856841bca0e8
youngstudent2 <644699280@qq.com>
2019-12-21 20:53:56 +0800

    Update Generator.v

--- Generator.v
+++ Generator.v
@@ -8,27 +8,7 @@ module Generator(clk,ch,speed,x,y);
 	 wire [11:0] ran_y;
 	 wire [7:0] ran_ch;
 	 wire [7:0] ran_speed;
-	 
-	 reg [9:0] count;
-	 reg [7:0] chcount;
-	 reg [7:0] scount;
-	 always @ (posedge clk) begin
-			if(count>=10'd630)
-				count<=10'd0;
-			else
-				count<=count+10'd9;
-				
-			if(chcount>=8'd26)
-				chcount<=8'd0;
-			else
-				chcount<=chcount+8'd1;
-				
-			if(scount>=8'd3)
-				scount<=8'd1;
-			else
-				scount<=8'd1+scount;
-	 end
-	/* 
+
 	 
     random8 #(2) random_speed(
         .clk(clk),
@@ -45,17 +25,13 @@ module Generator(clk,ch,speed,x,y);
 			.randomNum(ran_ch)
 	 );
 	
-	 */
-    //assign y = ran_y%70*9 ;  
+	 
+    assign y = ran_y%70*9 ;  
    
 	 assign x = 9'd0;
 
-	 //assign ch = 8'd65+ran_ch%26;
+	 assign ch = 8'd65+ran_ch%26;
 	 
-	 //assign speed = ran_speed%3 + 1;
+	 assign speed = ran_speed%3 + 1;
 
-	 assign y = count;
-	 assign ch = 8'd65+chcount;
-	 assign speed = scount;
-	 
 endmodule 
