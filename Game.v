e59c9a0d306848ae961c5418da54d3b27e4533df
youngstudent2 <644699280@qq.com>
2019-12-21 22:20:48 +0800

    Update Game.v
    我做了啥：
    对游戏时钟进行了限制
    加入了状态机制
    加入欢迎和结束状态界面，但是由于ROM过大，导致片内内存不足
    加入score的更新

--- Game.v
+++ Game.v
@@ -74,7 +74,7 @@ module Game(
 	reg [23:0] vga_data;
 	wire [9:0] h_addr;
 	wire [9:0] v_addr;
-
+	
 
 	//wire en;  //字符显存写入使能端
 	
@@ -105,7 +105,19 @@ module Game(
 	
 	reg [7:0] score;//分数
 	
+	//state contrl
+	wire [19:0] addr;
+	wire [11:0] wel_data;
+	wire [11:0] end_data;
+	reg clk_en;
+	reg reset;
+	wire dis_clk;
+
+	
 	initial begin
+		reset=1'b0;
+		clk_en=1'b0;
+		
 		gameover=1'b0;
 		count=6'd0;
 		countclk=19'd0;
@@ -120,15 +132,24 @@ module Game(
 	//生成vga_clk
 	clkgen #(25000000) my_vgaclk(
 		.clkin(CLOCK_50), 
-		.rst(SW[0]), 
+		.rst(1'b0), 
 		.clken(1'b1), 
 		.clkout(VGA_CLK) 
 	);
+	
+	//生成dis_clk
+	clkgen #(25000000) my_disclk(
+		.clkin(CLOCK_50), 
+		.rst(reset), 
+		.clken(clk_en), 
+		.clkout(dis_clk) 
+	);
+	
 	//用于随机生成字符的时钟
 	clkgen #(2) my_generator_clk(
 		.clkin(CLOCK_50), 
-		.rst(SW[0]), 
-		.clken(1'b1), 
+		.rst(reset), 
+		.clken(clk_en), 
 		.clkout(generator_clk) 
 	);
 	//随机生成字符
@@ -144,7 +165,7 @@ module Game(
 	ascii_ram ram(  //字符显存，写入char/读出get_asc
 		.data(char),
 		.rdaddress(outaddr),
-		.rdclock(VGA_CLK),
+		.rdclock(dis_clk),
 		.wraddress(inaddr),
 		.wrclock(generator_clk),
 		.wren(1'b1),
@@ -177,7 +198,7 @@ module Game(
 	assign rom_outaddr=get_asc_12bit + v_offset;
 	
 	//生成moveable
-	always @ (posedge VGA_CLK) begin 
+	always @ (posedge dis_clk) begin 
 		countclk=countclk+1'b1;
 		if(countclk==19'd420000)begin
 			count=count+6'd1;
@@ -192,7 +213,7 @@ module Game(
 		end
 	end
 	
-	always @ (posedge VGA_CLK) begin   //获取字符内列信息,no problem
+	always @ (posedge dis_clk) begin   //获取字符内列信息,no problem
 		if(columnTable[h_addr] == 1'b1) begin  //当前扫描处有新的字符
 			charIndex=h_addr;
 			h_offset=10'd0;
@@ -203,7 +224,7 @@ module Game(
 		end
 	end
 	
-	always @ (posedge VGA_CLK) begin   //获取字符内行信息,no problem
+	always @ (posedge dis_clk) begin   //获取字符内行信息,no problem
 		if(v_addr>=offset[charIndex]&&offset[charIndex]+4'd15>=v_addr) begin   
 			v_offset<=(v_addr-offset[charIndex])&10'b0000001111;
 			flag<=1'b1;
@@ -215,7 +236,7 @@ module Game(
 	end
 	
 	
-	always @ (posedge VGA_CLK) begin  //字符产生及下滑
+	always @ (posedge dis_clk) begin  //字符产生及下滑
 		if(generator_clk)begin  //生成字符，设置offset和speed
 			offset[tmp_y][8:0]<=tmp_x;
 			speed[tmp_y][2:0]<=tmp_speed;
@@ -228,7 +249,7 @@ module Game(
 				columnTable[charIndex]<=1'b0;
 				remove_flag<=1'b0;
 				//计分
-				score <= score + 8d'1;
+				score <= score + 8'd1;
 			end
 			else if(moveable==1'b1&&h_offset==10'd0&&v_addr==10'd0)begin  //该字符没有被消除，继续字符下滑
 				offset[charIndex]<=offset[charIndex]+speed[charIndex];
@@ -249,15 +270,17 @@ module Game(
 	always @ (posedge VGA_CLK) begin   //设置vga_data，显示
 			//界面状态机
 			case(state)
-				WEL_STATE: data <= wel_data;
+				WEL_STATE: vga_data <= 24'hff00ff;
+							//vga_data <= wel_data[11:8] << 20 | wel_data[7:4] << 12 | wel_data[3:0] << 4;
 				PLAY_STATE:     //游戏状态
 				begin
 					if((color_bit>>h_offset)&12'h001 == 1'b1) //取出的一位bit信息为1 
-						vga_data = white;  //white
+						vga_data <= white;  //white
 					else 
-						vga_data = black;  //black				
+						vga_data <= black;  //black				
 				end
-				END_STATE: data <= end_data;
+				END_STATE: vga_data <= 24'h00ffff; 
+							//vga_data <= end_data[11:8] << 20 | end_data[7:4] << 12 | end_data[3:0] << 4;
 			endcase
 			
 /*			
@@ -271,16 +294,31 @@ module Game(
 	
 	assign LEDR[0]=gameover;
 	
-	always @ (posedge KEY[0]) begin //状态转换
+	always @ (posedge CLOCK_50) begin //状态转换
 		case(state)
 			WEL_STATE:begin
-				
+			
+				if(press==8'd13) begin
+					clk_en <= 1'b1;
+					reset <= 1'b0;
+					state <= PLAY_STATE;
+					
+				end
 			end
 			PLAY_STATE:begin
-			
+				//考虑把score的更新移到这里
+								
+				if(gameover) begin
+					reset <= 1'b1;
+					state <= END_STATE;
+
+				end
 			end
 			END_STATE:begin
-				
+				clk_en <= 1'b0;
+				if(press==8'd13)
+					state <= WEL_STATE;
+					
 			end
 
 		endcase
@@ -291,10 +329,10 @@ module Game(
 	assign addr = h_addr<<9|(v_addr&9'b111111111);
 	
 	//欢迎界面
-	welROM welcome(addr,VGA_CLK,wel_data);
+	//welROM welcome(addr,VGA_CLK,wel_data);
 	
 	//结束界面
-	endROM gameend(addr,VGA_CLK,end_data);
+	//endROM gameend(addr,VGA_CLK,end_data);
 	
 	
 endmodule 
