65d4120b5dd4c89e57821b182e5edfe18702c27f
youngstudent2 <644699280@qq.com>
2019-12-22 17:35:48 +0800

    update

--- endROM.v
+++ endROM.v
@@ -43,7 +43,7 @@ module endROM (
 
 	input	[18:0]  address;
 	input	  clock;
-	output	[11:0]  q;
+	output	[2:0]  q;
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_off
 `endif
@@ -52,8 +52,8 @@ module endROM (
 // synopsys translate_on
 `endif
 
-	wire [11:0] sub_wire0;
-	wire [11:0] q = sub_wire0[11:0];
+	wire [2:0] sub_wire0;
+	wire [2:0] q = sub_wire0[2:0];
 
 	altsyncram	altsyncram_component (
 				.address_a (address),
@@ -71,7 +71,7 @@ module endROM (
 				.clocken1 (1'b1),
 				.clocken2 (1'b1),
 				.clocken3 (1'b1),
-				.data_a ({12{1'b1}}),
+				.data_a ({3{1'b1}}),
 				.data_b (1'b1),
 				.eccstatus (),
 				.q_b (),
@@ -83,16 +83,16 @@ module endROM (
 		altsyncram_component.address_aclr_a = "NONE",
 		altsyncram_component.clock_enable_input_a = "BYPASS",
 		altsyncram_component.clock_enable_output_a = "BYPASS",
-		altsyncram_component.init_file = "end.mif",
+		altsyncram_component.init_file = "../img/end.mif",
 		altsyncram_component.intended_device_family = "Cyclone V",
 		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
 		altsyncram_component.lpm_type = "altsyncram",
-		altsyncram_component.numwords_a = 327680,
+		altsyncram_component.numwords_a = 307200,
 		altsyncram_component.operation_mode = "ROM",
 		altsyncram_component.outdata_aclr_a = "NONE",
 		altsyncram_component.outdata_reg_a = "CLOCK0",
 		altsyncram_component.widthad_a = 19,
-		altsyncram_component.width_a = 12,
+		altsyncram_component.width_a = 3,
 		altsyncram_component.width_byteena_a = 1;
 
 
@@ -118,8 +118,8 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 // Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
-// Retrieval info: PRIVATE: MIFfilename STRING "end.mif"
-// Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "327680"
+// Retrieval info: PRIVATE: MIFfilename STRING "../img/end.mif"
+// Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "307200"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
 // Retrieval info: PRIVATE: RegOutput NUMERIC "1"
@@ -127,29 +127,29 @@ endmodule
 // Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 // Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
 // Retrieval info: PRIVATE: WidthAddr NUMERIC "19"
-// Retrieval info: PRIVATE: WidthData NUMERIC "12"
+// Retrieval info: PRIVATE: WidthData NUMERIC "3"
 // Retrieval info: PRIVATE: rden NUMERIC "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 // Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
-// Retrieval info: CONSTANT: INIT_FILE STRING "end.mif"
+// Retrieval info: CONSTANT: INIT_FILE STRING "../img/end.mif"
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone V"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
-// Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "327680"
+// Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "307200"
 // Retrieval info: CONSTANT: OPERATION_MODE STRING "ROM"
 // Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: OUTDATA_REG_A STRING "CLOCK0"
 // Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "19"
-// Retrieval info: CONSTANT: WIDTH_A NUMERIC "12"
+// Retrieval info: CONSTANT: WIDTH_A NUMERIC "3"
 // Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
 // Retrieval info: USED_PORT: address 0 0 19 0 INPUT NODEFVAL "address[18..0]"
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
-// Retrieval info: USED_PORT: q 0 0 12 0 OUTPUT NODEFVAL "q[11..0]"
+// Retrieval info: USED_PORT: q 0 0 3 0 OUTPUT NODEFVAL "q[2..0]"
 // Retrieval info: CONNECT: @address_a 0 0 19 0 address 0 0 19 0
 // Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
-// Retrieval info: CONNECT: q 0 0 12 0 @q_a 0 0 12 0
+// Retrieval info: CONNECT: q 0 0 3 0 @q_a 0 0 3 0
 // Retrieval info: GEN_FILE: TYPE_NORMAL endROM.v TRUE
 // Retrieval info: GEN_FILE: TYPE_NORMAL endROM.inc FALSE
 // Retrieval info: GEN_FILE: TYPE_NORMAL endROM.cmp FALSE
